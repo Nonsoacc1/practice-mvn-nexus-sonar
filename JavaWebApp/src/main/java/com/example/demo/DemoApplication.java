577265d35d361de9c6534a2aa43f08c9418e2f37
Nonsoacc1 <101227766+Nonsoacc1@users.noreply.github.com>
2022-10-25 19:07:57 -0600

    Update DemoApplication.java

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,11 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		Spring name = "nonoo";
-		System.out.printIn(name);
-		Intger age = "20";
-		System.out.printIn(age + 5);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "vamsi";
+        System.out.println(name);
+    }
 }
